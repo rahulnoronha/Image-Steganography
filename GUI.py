f92d5e2444929ecdea17838fe44a43ce6735d860
Arun Kumar <arunkumar.aqm@gmail.com>
2020-10-03 20:47:02 +0530

    Modify title design; add title to Frame1

--- GUI.py
+++ GUI.py
@@ -4,6 +4,9 @@ import cv2
 from Stenography import *
 from MergingImages import *
 
+BGD_COLOR = 'light blue'
+TITLE_BGD_COLOR = 'azure'
+TITLE_FORE_COLOR = 'red'
 
 global Frame1, Frame2, Frame3, Frame4, Home, frame
 global button1, button2, button3, button4, button5, button6, button7, button8, button9
@@ -14,27 +17,31 @@ message1 = ''
 window = Tk()
 window.title('Steganography')
 window.geometry("1000x1000")
-window.configure(bg='light blue')
+window.configure(bg=BGD_COLOR)
 
 Home = Frame(window)
 Frame1 = Frame(window)
 Frame2 = Frame(window)
 Frame3 = Frame(window)
 Frame4 = Frame(window)
-Home.configure(bg='light blue')
-Frame1.configure(bg='light blue')
-Frame2.configure(bg='light blue')
-Frame3.configure(bg='light blue')
-Frame4.configure(bg='light blue')
+Home.configure(bg=BGD_COLOR)
+Frame1.configure(bg=BGD_COLOR)
+Frame2.configure(bg=BGD_COLOR)
+Frame3.configure(bg=BGD_COLOR)
+Frame4.configure(bg=BGD_COLOR)
+
+def make_title(frame, text, bg = TITLE_BGD_COLOR, fg = TITLE_FORE_COLOR):
+    text_box = Text(frame, height=2, width=80, bg=bg, fg=fg, bd=5, state=DISABLED)
+    text_box.grid(row=200, column=1500)
+    myfont=("arial", 15, 'bold')
+    Label(frame, text = text.upper(), bg=bg, fg=fg, font=myfont).grid(row=200, column=1500)
 
 
 def home():
     ''' Home Frame '''
     global Home, Frame1, Frame2, Frame3, Frame4, text1, button1, button2, button3, button4, button5
-    Label(Home, text='', bg='light blue').grid(row=0, column=1500)
-    text1 = Text(Home, height=2, width=80, bg='blue', bd=5, state=DISABLED)
-    text1.grid(row=200, column=1500)
-    Label(Home, text='STEGANOGRAPHY PROGRAM', bg='blue', fg='white').grid(row=200, column=1500)
+    Label(Home, text='', bg=BGD_COLOR).grid(row=0, column=1500)
+    make_title(Home, 'Steganography Application')
     button1 = Button(Home, text='Hide Texts', bd=5, padx=30, pady=20, command=lambda: raise_frame(Frame1))
     button1.grid(row=400, column=500)
     button2 = Button(Home, text='Unhide Texts', bd=5, padx=30, pady=20, command=lambda: raise_frame(Frame3))
@@ -46,24 +53,31 @@ def home():
     button5 = Button(Home, text='Exit', bd=5, padx=30, pady=20, command=window.quit)
     button5.grid(row=2000, column=1500)
 
-
 def frame1():
     # Frame1 for Hiding Text
     global Frame1, Home
     global text2, button6, button7, button8, button9, clicked, message1, label1
     clicked = IntVar()
-    Label(Frame1, text='HIDE TEXT').grid(row=0, column=1500)
-    Label(Frame1, text='Hiding Text').grid(row=200, column=1500)
-    text2 = Text(Frame1, height=2, width=80, bd=5)
-    text2.grid(row=800, column=1500)
-    button6 = Button(Frame1, bd=5, text='Select Input Image Path', command=encode)
-    button6.grid(row=1600, column=1500)
-    Label(Frame1, text='Input the Data to be hidden first').grid(row=400, column=1500)
+    Label(Frame1, text='', bg=BGD_COLOR).grid(row=0, column=1500)
+    
+    make_title(Frame1, 'Hide Text')
+    
+    #Label(Frame1, text='Hiding Text').grid(row=400, column=1500)
+
+    text2 = Text(Frame1, height=4, width=60, bd=5)
+    text2.grid(row=1200, column=1500)
+
+    Label(Frame1, text='Input the Data to be hidden first').grid(row=800, column=1500)
+    
     button7 = Button(Frame1, bd=5, padx=10, pady=10, text='Enter', command=lambda: get_text)
-    button7.grid(row=1200, column=1500)
-    label1 = Button(Frame1, text=message1, padx=100, pady=100, state=DISABLED).grid(row=1300, column=1500)
+    button7.grid(row=1600, column=1500)
+    
+    button6 = Button(Frame1, bd=5, text='Select Input Image Path', command=encode)
+    button6.grid(row=1800, column=1500)
+
     button8 = Button(Frame1, bd=5, text='Go to Home', padx=30, pady=20, command=lambda: raise_frame(Home))
     button8.grid(row=5000, column=400)
+    
     button9 = Button(Frame1, bd=5, text='Clear', padx=30, pady=20, command=lambda: clear_frame(Frame1))
     button9.grid(row=5000, column=5000)
 
@@ -71,9 +85,8 @@ def frame1():
 def frame2():
     """ Frame2 for Hiding Image """
     global Home, Frame2, clicked, text3, drop, button10, button11, button12
-    Label(Frame2, text='', bg='light blue').grid(row=0, column=1500)
-    text3 = Text(Frame2, height=2, width=80, bg='red', bd=5, state=DISABLED).grid(row=200, column=1500)
-    Label(Frame2, text='HIDE IMAGE', fg='black', bg='red').grid(row=200, column=1500)
+    Label(Frame2, text='', bg=BGD_COLOR).grid(row=0, column=1500)
+    make_title(Frame2, "Hide Image")
     Label(Frame2, text='Hiding Image').grid(row=400, column=1500)
     Label(Frame2, text='Number of bits to use for Hiding').grid(row=800, column=1500)
     drop = OptionMenu(Frame2, clicked, '1', '2', '3', '4', '5', '6', '7')
@@ -89,9 +102,8 @@ def frame2():
 def frame3():
     """ Frame3 for Retrieving Text """
     global Home, text3, Frame3, button13, button14, button15, message, label2
-    Label(Frame3, text='', bg='light blue').grid(row=0, column=1500)
-    text3 = Text(Frame3, height=2, width=80, bd=5, bg='green', state=DISABLED).grid(row=200, column=1500)
-    Label(Frame3, text='RETRIEVE TEXT', bg='green', fg='black').grid(row=200, column=1500)
+    Label(Frame3, text='', bg=BGD_COLOR).grid(row=0, column=1500)
+    make_title(Frame3, "Retrieve Text")
     Label(Frame3, text='Retrieving Text').grid(row=600, column=1500)
     Label(Frame3, text='Select Image Path').grid(row=400, column=1500)
     button13 = Button(Frame3, bd=5, padx=10, pady=10, text='Enter', command=decode)
@@ -107,16 +119,16 @@ def frame4():
     """ Frame4 for Retrieving Image """
     global Home, clicked1, Frame4, text4, drop1, button16, button17, button18
     clicked1 = IntVar()
-    Label(Frame4, text='', bg='light blue').grid(row=0, column=2500)
-    text4 = Text(Frame4, height=2, width=80, bd=5, bg='green').grid(row=200, column=2500)
-    Label(Frame4, text='RETRIEVE IMAGE', fg='black', bg='green').grid(row=200, column=2500)
-    Label(Frame4, text='Select Image Path').grid(row=400, column=2500)
-    Label(Frame4, text='Retrieving Image').grid(row=600, column=2500)
-    Label(Frame4, text='Number of bits that were use for Hiding').grid(row=800, column=2500)
+    Label(Frame4, text='', bg=BGD_COLOR).grid(row=0, column=1500)
+
+    make_title(Frame4, "Retrieve Image")
+    Label(Frame4, text='Select Image Path').grid(row=400, column=1500)
+    Label(Frame4, text='Retrieving Image').grid(row=600, column=1500)
+    Label(Frame4, text='Number of bits that were use for Hiding').grid(row=800, column=1500)
     drop1 = OptionMenu(Frame4, clicked1, '1', '2', '3', '4', '5', '6', '7')
-    drop1.grid(row=1000, column=2500)
+    drop1.grid(row=1000, column=1500)
     button16 = Button(Frame4, bd=5, padx=10, pady=10, text='Unhide', command=unmerge)
-    button16.grid(row=1400, column=2500)
+    button16.grid(row=1400, column=1500)
     button17 = Button(Frame4, bd=5, text='Go to Home', padx=30, pady=20, command=lambda: raise_frame(Home))
     button17.grid(row=2000, column=200)
     button18 = Button(Frame4, bd=5, text='Clear', padx=30, pady=20, command=lambda:clear_frame(Frame4))
@@ -189,7 +201,7 @@ def encode():
     image1=cv2.imread(input_image_path)
     if image1 is None:
         raise FileNotFoundError()
-    display_image(image1, 'Resized Input Image')
+    display_image(image1, 'Input Image, Resized to Fit')
     data = get_text()
     if len(data) == 0:
         raise ValueError('Data is empty')
@@ -208,10 +220,10 @@ def decode():
     input_image_path = file_select()
     image = cv2.imread(input_image_path)  # Read the input image using OpenCV-Python.
 
-    display_image(image, 'Resized Image with Hidden text')
+    display_image(image, 'Resultant Image, Resized to Fit')
 
     text = unhide_data(image)
-    message='Decoded message is ' + text
+    message='Decoded message is \n' + text
     reset_frame(Frame3)
     message=''
 
@@ -227,7 +239,7 @@ def merge():
     result = merge_images(pub_image, secret_image, nf_bits)
     output_image_path = file_save()
     cv2.imwrite(output_image_path, result)
-    display_image(result, "Merged and Saved")
+    display_image(result, "Resultant Image, Resized to Fit")
 
 
 def unmerge():
